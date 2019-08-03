850cf57b454e42a456ade1ea8205bd1aa92fbe16
amlandy <amlandy@umich.edu>
2019-08-02 22:00:43 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      response: {},
+      data: [],
+      name: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getValues = this.getValues.bind(this);
+  }
+
+  componentDidMount() {
+    this.getValues();
+  }
+
+  getValues(){
+    const alpha = require('alphavantage')({ key: '08Q0YI6I3581QAAU' });
+    alpha.data.daily(this.state.name.toString())
+    .then(response => {
+      console.log(response);
+      this.setState({
+        name: response['Meta Data']['2. Symbol'],
+        data: Object.values(response['Time Series (Daily)'])
+      });
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  }
+
+  handleSubmit(value){
+    console.log("Handle Submit", value);
+    this.setState({
+      name: value,
+    })
+    this.getValues();
+  }
+
+
+  render(){
+    return(
+      <div>
+        <input type="text" name="ticker"
+          onKeyPress={event => {
+            if(event.key === 'Enter') {
+              this.handleSubmit(event.target.value)
+            }
+          }}
+        />
+
+        <h1>{this.state.name}</h1>
+        <div>
+          {this.state.data.map((item) => (
+            <p> {item['1. open']} </p>
+          ))}
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
